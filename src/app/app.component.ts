888da794b0ccf123df9fda1889150ef65e57a31d
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-08 23:00:38 -0300

    estructura del tp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Punto1Component } from "./punto1/punto1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Punto1Component]
 })
 export class AppComponent {
   title = 'practicaAngular2024';
