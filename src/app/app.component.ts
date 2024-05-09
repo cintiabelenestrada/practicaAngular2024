76c2e8a99ecf8e834c0637de3b549c5fa0800d54
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-09 02:57:04 -0300

    agrego mas components header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Punto1Component } from "./punto1/punto1.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Punto1Component]
+    imports: [RouterOutlet, Punto1Component, FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'practicaAngular2024';
