e21776dfb00c5f0b23b38499f91837ef6169a91d
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-09 04:46:19 -0300

    imagenes del vector producto

--- src/app/punto1/punto1.component.ts
+++ src/app/punto1/punto1.component.ts
@@ -12,12 +12,12 @@ export class Punto1Component implements OnInit {
   title: string = 'tDISCO SOLIDO SSD 240GB ADATA SATA III SU630';
 // defini una variable title que luego la llamo en la vista
   productos=  [
-    { nombre: 'Auricular Gamer Logitech', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'notebook13l.jpg', precio: 4500.5},
-    { nombre: 'JOYSTICK  PC USB G807', descripcion: 'El Saturn es el joystick cableado ideal, analógicos, vibración y compatibilidad son solo algunas de sus características.', img: '/punto1/img/joystick.png', precio: 2000.99},
-    { nombre: 'Monitor Gamer 22"', descripcion: 'Color : NegroTamaño de panel : 22"Curvo/Plano : PlanoColores : 16.7MTipo de panel : IPSResolución : 1920 x 1080s', img: 'notebook13l.jpg', precio: 200.999},
-    { nombre: 'Cooler Cpu Watercooler Gamemax Iceburg 360 Rainbow 295W', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'notebook13l.jpg', precio: 4500.5},
-    { nombre: 'Auricular Gamer Logitech', descripcion: 'Conector RGB para ventilador : Conector 2510 Doble AURA 5V 3 pinesRPM del ventilador : 800 ,2000 RPMFlujo de aire del ventilador : 62-82CFMMaterial del tubo : Caucho de baja evaporaciónTipo Sistema de refrigeración : por agua de CPU', img: 'notebook13l.jpg', precio: 19900.5},
-    { nombre: 'Auricular Gamer Logitech', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'notebook13l.jpg', precio: 4500.5}
+    { nombre: 'Auricular Gamer Logitech', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'audicular.webp', precio: 4500.5},
+    { nombre: 'JOYSTICK  PC USB G807', descripcion: 'El Saturn es el joystick cableado ideal, analógicos, vibración y compatibilidad son solo algunas de sus características.', img: 'joystick.jpeg', precio: 2000.99},
+    { nombre: 'Monitor Gamer 22"', descripcion: 'Color : NegroTamaño de panel : 22"Curvo/Plano : PlanoColores : 16.7MTipo de panel : IPSResolución : 1920 x 1080s', img: 'monitor.jpeg', precio: 200.999},
+    { nombre: 'Cooler Cpu Watercooler Gamemax Iceburg 360 Rainbow 295W', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'cooler.jpeg', precio: 4500.5},
+    { nombre: 'Teclado Gamer HyperX Alloy Origins', descripcion: 'Tamaño del enter : GrandeRetroiluminación del teclado : RGB', img: 'teclado.jpeg', precio: 10000.99},
+    { nombre: 'Tableta Gráfica Xp Pen Deco Pro Small', descripcion: 'Sensibilidad a la presion : 8192 nivelesResolucion : 5080 LPITasa de informe : Max 200Rueda mecánica : 1', img: 'tablagrafica.jpeg', precio: 129000.99}
   ];
 
  constructor() { }
